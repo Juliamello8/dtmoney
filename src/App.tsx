883c6475edc06351361758b3e443d370ad6db74c
Juliamello8 <juliainline@gmail.com>
2021-05-05 16:44:26 -0300

    Criando próprio hook para transições

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Modal from 'react-modal';
 
 import { GlobalStyle } from './styles/global'
 import { NewTransactionModal } from './components/NewTransactionModal/Index';
-import { TransactionsProvider } from './contexts/TransactionsContext';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 //adiciona o modal na div root e não no início do html e melhora acessibilidade
 Modal.setAppElement('#root');
@@ -22,7 +22,7 @@ export function App() {
     }
 
     return (
-        <TransactionsProvider>
+        <TransactionsProvider >
             <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
             <Dashboard />
             <NewTransactionModal
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState, ReactNode } from 'react'
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react'
 import { api } from '../services/api'
 
 interface Transaction {
@@ -23,7 +23,7 @@ interface TransactionsContextData {
     createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
 );
 
@@ -55,3 +55,8 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     )
 }
 
+export function useTransactions() {
+    const context = useContext(TransactionsContext)
+
+    return context
+}
